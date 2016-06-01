101b3fe3c108dbef52b1d467c0e576914298cf9a
bigsuperyu <bigsuperangel@gmail.com>
2016-06-01 15:17:44 +0800

    new page

---
+++ app/pages/my-page/my-page.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the MyPagePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/my-page/my-page.html',
+})
+export class MyPagePage {
+  constructor(public nav: NavController) {}
+}
